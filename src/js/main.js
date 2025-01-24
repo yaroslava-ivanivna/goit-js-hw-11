3bb31b80765c6dcf6f7128c94b915c6773cce647
Yaroslava <yaroslava.ivanivna9@gmail>
2025-01-24 21:47:17 +0200

    almost ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,30 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const loader = document.querySelector('.loader');
+function showLoader() {
+  loader.classList.remove('visually-hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('visually-hidden');
+}
+
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryList = document.querySelector('.js-gallery');
 
 const createGalleryCardTemplate = imageInfo => {
   return `<li class="gallery-card">
-   <img
+  <a class="gallery-link-image" href="${imageInfo.largeImageURL}"> 
+      <img
       class="gallery-image"
       src="${imageInfo.webformatURL}"
       data-source="${imageInfo.largeImageURL}"
       alt="${imageInfo.tags}"
-    />
-  
+    /> </a>
+
    <div class="image-info">
       <p class="image-additional-info"><strong>Likes:</strong> ${imageInfo.likes}</p>
       <p class="image-additional-info"><strong>Views:</strong> ${imageInfo.views}</p>
@@ -22,7 +37,20 @@ const createGalleryCardTemplate = imageInfo => {
 const onSearchFormSubmit = event => {
   event.preventDefault();
 
-  const searchFormValue = event.currentTarget.elements.search.value;
+  showLoader();
+
+  const searchFormValue = event.currentTarget.elements.search.value.trim();
+  if (searchFormValue === '') {
+    iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+    searchFormEl.removeEventListener('submit', onSearchFormSubmit);
+    return;
+  }
+
   fetch(
     `https://pixabay.com/api/?key=48347976-46935637adedce2affc2ad0dc&q=${searchFormValue}&image_type=photo&orientation=horizontal$safesearch=true`
   )
@@ -30,24 +58,39 @@ const onSearchFormSubmit = event => {
       if (!response.ok) {
         throw new Error(response.status);
       }
+
       return response.json();
     })
     .then(data => {
+      if (data.total === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        galleryList.innerHTML = '';
+        searchFormEl.reset();
+        return;
+      }
       const galleryTemplate = data.hits
         .map(el => createGalleryCardTemplate(el))
         .join('');
 
       galleryList.innerHTML = galleryTemplate;
-    })
-    .catch(err => console.log(err));
-};
-searchFormEl.addEventListener('submit', onSearchFormSubmit);
 
-// <h2 class="likes-title js-likes-title">Likes</h2>
-// <p class="likes-info js-likes-info"></p>
-// <h2 class="views-title js-views-title">Views</h2>
-// <p class="views-info js-views-info"></p>
-// <h2 class="comments-title js-comments-title">Coments</h2>
-// <p class="comments-info js-comments-info"></p>
-// <h2 class="downloads-title js-downloads-title">Downloads</h2>
-// <p class="downloads-info js-downloads-info"></p>
+      hideLoader();
+      searchFormEl.addEventListener('submit', onSearchFormSubmit);
+
+      new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      hideLoader();
+      searchFormEl.addEventListener('submit', onSearchFormSubmit);
+    });
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
